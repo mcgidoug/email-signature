deeb54be2b9ad9371cbf1d0ae9c26ef3713a78b9
mcgidoug <mcgidoug@gmail.com>
2023-07-24 18:12:51 -0500

    finished basic functions, set for refactoring

--- app/components/Generator.jsx
+++ app/components/Generator.jsx
@@ -8,6 +8,13 @@ const Generator = () => {
   const [numberText, setNumberText] = useState("");
   const [emailText, setEmailText] = useState("");
 
+  const [linkedinText, setLinkedinText] = useState("");
+  const [facebookText, setFacebookText] = useState("");
+  const [instagramText, setInstagramText] = useState("");
+  const [youtubeText, setYoutubeText] = useState("");
+  const [githubText, setGithubText] = useState("");
+
+  // GENERAL ================================================================
   const handleNameText = (event) => {
     const newNameText = event.target.value;
     setNameText(newNameText);
@@ -28,8 +35,36 @@ const Generator = () => {
     setEmailText(newEmailText);
   };
 
+  // SOCIALS ================================================================
+
+  const handleLinkedinText = (event) => {
+    const newLinkedinText = event.target.value;
+    setLinkedinText(newLinkedinText);
+  };
+
+  const handleFacebookText = (event) => {
+    const newFacebookText = event.target.value;
+    setFacebookText(newFacebookText);
+  };
+
+  const handleInstagramText = (event) => {
+    const newInstagramText = event.target.value;
+    setInstagramText(newInstagramText);
+  };
+
+  const handleYoutubeText = (event) => {
+    const newYoutubeText = event.target.value;
+    setYoutubeText(newYoutubeText);
+  };
+
+  const handleGithubText = (event) => {
+    const newGithubText = event.target.value;
+    setGithubText(newGithubText);
+  };
+
   return (
     <div>
+      {/* GENERAL ================================================================ */}
       <div className="flex">
         <div className="p-5">
           <h1 className="text-2xl mb-2 ">Basic Info</h1>
@@ -63,6 +98,7 @@ const Generator = () => {
           />
         </div>
 
+        {/* SOCIALS ================================================================ */}
         <div className="p-5">
           <h1 className="text-2xl mb-2">Socials</h1>
           <h2>LinkedIn</h2>
@@ -70,27 +106,36 @@ const Generator = () => {
             className="text-black"
             type="text"
             placeholder="Your LinkedIn"
+            onChange={handleLinkedinText}
           />
           <h2>Facebook</h2>
           <input
             className="text-black"
             type="text"
             placeholder="Your Facebook"
+            onChange={handleFacebookText}
           />
           <h2>Instagram</h2>
           <input
             className="text-black"
             type="text"
             placeholder="Your Instagram"
+            onChange={handleInstagramText}
           />
           <h2>YouTube</h2>
           <input
             className="text-black"
             type="text"
             placeholder="Your YouTube"
+            onChange={handleYoutubeText}
           />
           <h2>Github</h2>
-          <input className="text-black" type="text" placeholder="Your Github" />
+          <input
+            className="text-black"
+            type="text"
+            placeholder="Your Github"
+            onChange={handleGithubText}
+          />
         </div>
       </div>
 
@@ -101,6 +146,13 @@ const Generator = () => {
           <div>{websiteText}</div>
           <div>{numberText}</div>
           <div>{emailText}</div>
+        </div>
+        <div className="bg-white text-black w-1/2 h-40 border-2 overflow-auto grid-cols-1">
+          <div>{linkedinText}</div>
+          <div>{facebookText}</div>
+          <div>{instagramText}</div>
+          <div>{youtubeText}</div>
+          <div>{githubText}</div>
         </div>
         <div className="flex gap-x-2">
           <div className="bg-white text-black my-2 p-2 cursor-pointer">Run</div>
